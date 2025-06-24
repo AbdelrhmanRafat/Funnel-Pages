648c64404043b635528ff4b058cefa8cc8622183
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2025-06-24 12:33:06 +0300

    add enviroment.ts

---
+++ src/Enviroment/Local.enviroment.ts
@@ -0,0 +1,13 @@
+export const baseUrl  = "https://baseet.co/api.php?id=1"
+export const headers = {
+      "Accept": "application/json",
+      "Accept-Language": "en-US",
+      "App-Version": "1.0.0",
+      "Device-Name": "iPhone14,7",
+      "Device-OS-Version": "17.4",
+      "Device-UDID": "123e4567-e89b-12d3-a456-426614174000",
+      "Device-Push-Token": "a1b2c3d4e5f6g7h8i9",
+      "Device-Type": "ios",
+      "country-code": "SA",
+      "Cookie": "laravel_session=abcdef1234567890"
+}
